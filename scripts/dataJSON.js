3c9d552fb6b0f75dc7df5a7a2a0fb9512e3bddfa
kevinsaliu <57330334+kevinsaliu@users.noreply.github.com>
2020-01-15 19:13:17 +0100

    fotos and landscape

--- scripts/dataJSON.js
+++ scripts/dataJSON.js
@@ -31,77 +31,20 @@ let dataJSON = [
         {"x": 261, "y": 426}  // 7. Hauptbuch Farbe 
       ]
     },
-    {
+	{
       "type": "Image",
-      "imgOriginalPath": "assets/imgForPlay/helden_orig.png",
-      "imgClonePath": "assets/imgForPlay/helden_copy.png",
-      "imgWidth": 636,
-      "imgHigh": 715,
+      "imgOriginalPath": "assets/imgForPlay/dog_orig.png",
+      "imgClonePath": "assets/imgForPlay/dog_copy.png",
+      "imgWidth": 1000,
+      "imgHigh": 1100,
       "coordinatesOfDifferences": [
-        {"x": 480, "y": 348},	// 1. Held Rosa Falte
-        {"x": 165, "y": 204}, // 2. Mädchen P
-        {"x": 90, "y": 364},	// 3. Haus vorne Fenster oben
-        {"x": 168, "y": 359}, // 4. Rosanes Haus
-        {"x": 344, "y": 386},  // 5. Hund Schnurbard
-        {"x": 281, "y": 290},  // 6. Junge Schuh
-        {"x": 214, "y": 300},  // 7. Hase Feuer
-        {"x": 273, "y": 482},  // 8. Graues Haus + Fenster
-        {"x": 255, "y": 154},  // 9. Fuchs Nase
-        {"x": 371, "y": 371}  // 10. Hund Maske 
-      ]
-    },
-    {
-      "type": "Image",
-      "imgOriginalPath": "assets/imgForPlay/bauernhof_orig.png",
-      "imgClonePath": "assets/imgForPlay/bauernhof_copy.png",
-      "imgWidth": 669,
-      "imgHigh": 732,
-      "coordinatesOfDifferences": [
-        {"x": 235, "y": 509},	// 1. Träger
-        {"x": 534, "y": 361}, // 2. Haus Fenster
-        {"x": 354, "y": 561},	// 3. Junge vorne Haare
-        {"x": 552, "y": 435}, // 4. Pferd Fleck
-        {"x": 601, "y": 357},  // 5. Pferd Schwanz
-        {"x": 56, "y": 552},  // 6. Hähnchen Flügel
-        {"x": 618, "y": 482},  // 7. Holz
-        {"x": 379, "y": 288},  // 8. Schwein Blume
-        {"x": 339, "y": 106},  // 9. Hund Halsband
-        {"x": 51, "y": 343}  // 10. Wolke  
-      ]
-    },
-    {
-      "type": "Image",
-      "imgOriginalPath": "assets/imgForPlay/skulptur_orig.png",
-      "imgClonePath": "assets/imgForPlay/skulptur_copy.png",
-      "imgWidth": 600,
-      "imgHigh": 750,
-      "coordinatesOfDifferences": [
-        {"x": 144, "y": 628},	// 1. Skulptur Fuss
-        {"x": 229, "y": 472}, // 2. Skulptur Zähne
-        {"x": 419, "y": 418},	// 3. Hose
-        {"x": 429, "y": 352}, // 4. T-shirt
-        {"x": 343, "y": 423},  // 5. Tasche Hose
-        {"x": 203, "y": 587},  // 6. Skulptur Fuss
-        {"x": 114, "y": 284},  // 7. Skulptur Stock
-        {"x": 159, "y": 181},  // 8. Hammer
-        {"x": 275, "y": 432},  // 9. Skulptur Nase
-        {"x": 372, "y": 285}  // 10. Zunge  
-      ]
-    },
-    {
-      "type": "Image",
-      "imgOriginalPath": "assets/imgForPlay/mark_orig.png",
-      "imgClonePath": "assets/imgForPlay/mark_copy.png",
-      "imgWidth": 716,
-      "imgHigh": 792,
-      "coordinatesOfDifferences": [
-        {"x": 300, "y": 650},	// 1. Schuhe
-        {"x": 96, "y": 554}, // 2. Buch Punkt
-        {"x": 629, "y": 475},	// 3. Buch Streifen
-        {"x": 417, "y": 675}, // 4. Lesezeichen
-        {"x": 374, "y": 196},  // 5. Augenbraun
-        {"x": 240, "y": 562},  // 6. Buch Seiten
-        {"x": 261, "y": 426}  // 7. Hauptbuch Farbe 
+        {"x": 612, "y": 800},	// 1. Fehlende Falte am Pfote
+        {"x": 513, "y": 499}, // 2. Halsband
+        {"x": 100, "y": 887},	// 3. Blumenfarbe
+        {"x": 800, "y": 1027}, // 4. Fehlende Blume
+        {"x": 659, "y": 431},  // 5. Fehlender Punkt
+        {"x": 403, "y": 917},  // 6. Fehlende Linie
+        {"x": 515, "y": 115},  // 7. Farbe der Schnabel 
       ]
     },
     {
@@ -160,7 +103,74 @@ let dataJSON = [
         {"x": 275, "y": 432},  // 9. Skulptur Nase
         {"x": 372, "y": 285}  // 10. Zunge  
       ]
-    }
+    },
+    {
+      "type": "Image",
+      "imgOriginalPath": "assets/imgForPlay/jeep_orig.png",
+      "imgClonePath": "assets/imgForPlay/jeep_copy.png",
+      "imgWidth": 700,
+      "imgHigh": 800,
+      "coordinatesOfDifferences": [
+        {"x": 501, "y": 23},	// 1. Hut
+        {"x": 460, "y": 166}, // 2. Schnurbart
+        {"x": 422, "y": 333},	// 3. Lenkrad
+        {"x": 540, "y": 205}, // 4. Halsband
+        {"x": 322, "y": 161},  // 5. Fernglas
+        {"x": 655, "y": 610},  // 6. Rad hinten links
+        {"x": 81, "y": 640},  // 7. Rad vorne rechts
+		{"x": 37, "y": 469},	// 8. Zebralinie
+        {"x": 299, "y": 443}, // 9. Scheinwerfer
+        {"x": 197, "y": 577},  // 10. Kennschild
+        {"x": 534, "y": 483},  // 11. Auto Seitenfarbe
+        {"x": 446, "y": 436},  // 12. Zebralinie
+		{"x": 188, "y": 469}  // 13. Logo 
+      ]
+    },
+    {
+      "type": "Image",
+      "imgOriginalPath": "assets/imgForPlay/galaxy_orig.png",
+      "imgClonePath": "assets/imgForPlay/galaxy_copy.png",
+      "imgWidth": 700,
+      "imgHigh": 800,
+      "coordinatesOfDifferences": [
+        {"x": 168, "y": 486},	// 1. Junge umgedreht
+        {"x": 251, "y": 633}, // 2. Ufo Licht
+        {"x": 354, "y": 524},	// 3. Stiefelfarbe
+        {"x": 485, "y": 580}, // 4. ufo fehlt
+        {"x": 387, "y": 296},  // 5. haarfarbe
+        {"x": 291, "y": 427},  // 6. steinfarbe
+        {"x": 499, "y": 303},  // 7. Farbe
+		{"x": 311, "y": 288},	// 8. Ein Auge nur
+        {"x": 447, "y": 219}, // 9. Stein extra
+        {"x": 115, "y": 368},  // 10. Farbe
+        {"x": 277, "y": 219},  // 11. Schuhsohle
+        {"x": 232, "y": 331},  // 12. Nase des Hundes
+		{"x": 418, "y": 518}  // 13. Jetpack
+      ]
+    },
+    {
+      "type": "Image",
+      "imgOriginalPath": "assets/imgForPlay/zeitreise_orig.png",
+      "imgClonePath": "assets/imgForPlay/zeitreise_copy.png",
+      "imgWidth": 600,
+      "imgHigh": 700,
+      "coordinatesOfDifferences": [
+        {"x": 365, "y": 180},	// 1. Stern
+        {"x": 473, "y": 469}, // 2. Krone
+        {"x": 358, "y": 486},	// 3. Mond auf dem Stein
+        {"x": 131, "y": 269}, // 4. Fehlende Säule am Totem
+        {"x": 352, "y": 277},  // 5. Falten am Stirn
+        {"x": 263, "y": 447},  // 6. Fehlende Buttons
+        {"x": 177, "y": 435},  // 7. Totemfarbe
+		{"x": 169, "y": 374},	// 8. Totem Zähne
+        {"x": 289, "y": 314}, // 9. Fehlende Reflektion
+        {"x": 345, "y": 371},  // 10. Krawattenfarbe
+        {"x": 416, "y": 427},  // 11. Knopffarbe
+        {"x": 212, "y": 299},  // 12. Schildfarbe
+		{"x": 274, "y": 199}  // 13. Speerkopf
+      ]
+    
+	},
 ];
 
 
